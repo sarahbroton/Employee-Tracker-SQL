b6da3471e9245cd4cc2161780ee66158c2b6af21
sarahbroton <sarah.broton@gmail.com>
2023-07-25 15:21:44 -0500

    updateschema

---
+++ query.sql
@@ -0,0 +1,11 @@
+SELECT department_id 
+FROM role
+JOIN  ON ?
+
+
+-- ORDER BY ???
+-- LEFT JOIN???
+
+-- SELECT ?
+-- FROM table_name; 
+-- JOIN reference ON same_table
--- schema.sql
+++ schema.sql
@@ -4,33 +4,32 @@ CREATE DATABASE etracker_db;
 USE etracker_db;
 
 CREATE TABLE department (
-     id INT NOT NULL PRIMARY KEY,
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR(30) NOT NULL, 
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 );  
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
-    role_id INT,
-    manager_id int NOT NULL
+    role_id INT NOT NULL,
+    manager_id INT, 
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 ); 
    
  
 --  MANAGER_ID LINKS TO ID IN THE EMPLOYEE TABLE
--- line 8 'name'
--- was just name
--- line 28 'role'
+
+
