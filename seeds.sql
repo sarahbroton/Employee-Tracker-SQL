92582df76a0cc6746d2f9934c0a815dd0e053c8d
sarahbroton <sarah.broton@gmail.com>
2023-07-25 15:42:16 -0500

    seedupdate

--- seeds.sql
+++ seeds.sql
@@ -1,32 +1,38 @@
-INSERT INTO department (department_name, department_id)
-VALUES ("Sales" ),
+INSERT INTO department (department_name)
+VALUES ("Sales"),
        ("Engineering"),
+       ("IT/IS"), 
        ("Finance"),
+       ("Accounting"), 
        ("Legal"),
-       ("Customer Service");
+       ("Customer Service"),
+       ("Human Resources"); 
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Salesperson", 80000, 2),
-       ("Lead Engeineer', 150000, 3), 
-       ("Software Engineer", 120000, 4), 
-       ("Account Manager", 16000, 5), 
-       ("Accountant", 125000, 6), 
-       ("Legal Team Lead", 250000 7), 
-       ("Lawyer", 190000, 8); 
+VALUES ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2), 
+       ("Software Engineer", 120000, 3), 
+       ("Account Manager", 16000, 4), 
+       ("Accountant", 125000, 4), 
+       ("Legal Team Lead", 250000, 5), 
+       ("Lawyer", 190000, 5); 
+       ("Front Desk", 1000000, 6), 
+       ("HR", 50000, 7); 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jim", "Halpert), 
-       ("Pam", "Beesley), 
-       ("Andy", "Bernard), 
-       ("Stanley", "Hudson), 
-       ("Kevin", "Malone), 
-       ("Michael", "Scott), 
-        ("Meredith", "Palmer), 
-        ("Angela", "Martin), 
-        ("Oscar", "Martinez), 
-        ("Phyllis", "Vance), 
-        ("Toby", "Glenderson), 
-        ("Creed", "Bratton); 
+VALUES ("Michael", "Scott", 1, NULL), 
+        ("Jim", "Halpert", 1, 1), 
+       ("Pam", "Beesley", 6, 1), 
+       ("Andy", "Bernard", 1, 1), 
+       ("Stanley", "Hudson", 5, 1), 
+       ("Kevin", "Malone", 5, 1), 
+       ("Meredith", "Palmer", 4, 1 ), 
+       ("Angela", "Martin", 5, 1), 
+       ("Oscar", "Martinez", 5, 1), 
+       ("Phyllis", "Vance", 1, 1), 
+       ("Toby", "Glenderson", 7, NULL), 
+       ("Creed", "Bratton", 7, 1), 
+       ("Dwight", "Shrutte", 1, 1); 
        
 
      
